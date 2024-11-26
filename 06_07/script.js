32a5fe74a9d816a54922653c469d79fa060af948
Q Watts <54119641+qwatts-dev@users.noreply.github.com>
2024-11-25 23:29:58 -0500

    JavaScript Essential Training Chapter 6

--- 06_07/script.js
+++ 06_07/script.js
@@ -9,7 +9,7 @@ let b = 4;
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 
-if (a == b) {
+if (a != b) {
   console.log(`Match! let a and let b are the same value.`);
 } else {
   console.error(`No match: let a and let b are NOT same value.`);
--- 06_08/script.js
+++ 06_08/script.js
@@ -3,14 +3,14 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators#Arithmetic_operators
  */
 
-let a = 5;
-let b = 4;
+let a = 15;
+let b = "4";
 let c = 3.2;
 
 console.log(`let a: ${a} (${typeof a})`);
 console.log(`let b: ${b} (${typeof b})`);
 console.log(`let c: ${c} (${typeof c})`);
 
-let result = a + b;
+let result = a - b;
 
 console.log("Result: ", result);
