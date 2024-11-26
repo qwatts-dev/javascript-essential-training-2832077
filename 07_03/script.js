7c4e58e940329c9f8a3a781fa122d6a4efa22c75
Q Watts <54119641+qwatts-dev@users.noreply.github.com>
2024-11-26 00:32:28 -0500

    JavaScript Essential Training Chapter 7

--- 07_03/script.js
+++ 07_03/script.js
@@ -3,18 +3,18 @@
  * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array#Instance_methods
  */
 
-let backpackContents = ["piggy", "headlamp", "pen"];
-
-console.log(backpackContents);
+let backpackContents = ["pig", "headlamp", "pen"];
 
 // backpackContents.forEach(function (item) {
 //   item = `<li>${item}</li>`;
 //   console.log(item);
 // });
 
-// let longItems = backpackContents.find(function (item) {
-//   if (item.length >= 5) {
-//     return item;
-//   }
-// });
-// console.log("longItems:", longItems);
+let longItems = backpackContents.find(function (item) {
+  if (item.length >= 5) {
+    return item;
+  }
+});
+console.log("longItems:", longItems);
+
+console.log(backpackContents);
